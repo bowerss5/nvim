0883db9badc24185f281e23580f454ebde1c7181
bowerss5 <bowerss5@miamioh.edu>
2025-01-22 01:53:23 -0600

    Added bind for todotelescope

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -46,3 +46,5 @@ vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
 vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts) -- close buffer
 vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
+
+vim.keymap.set("n", "<leader>st", "<cmd> TodoTelescope <CR>", opts)
