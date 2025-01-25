eb421c27ac93ce1d94df875008773b271b789c4f
bowerss5 <bowerss5@miamioh.edu>
2025-01-25 04:28:33 -0600

    Added Leetcode

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -40,4 +40,16 @@ return {
 			require("colorizer").setup()
 		end,
 	},
+	{
+		"kawre/leetcode.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			-- "ibhagwan/fzf-lua",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+		opts = {
+			-- configuration goes here
+		},
+	},
 }
